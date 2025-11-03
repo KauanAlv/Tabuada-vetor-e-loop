e56e4bcbadc835c2cf21f528e810b2faf5ecc8a2
KAUAN ALVES PEREIRA <25203696@CFP127ALUNO.LOCAL>
2025-11-03 16:09:13 -0300

    Obter informacoes finalizado

--- src/br/senai/sp/jandira/tabuada/TabuadaApp.java
+++ src/br/senai/sp/jandira/tabuada/TabuadaApp.java
@@ -1,4 +1,12 @@
 package br.senai.sp.jandira.tabuada;
 
+import br.senai.sp.jandira.tabuada.model.Usuario;
+
 public class TabuadaApp {
+    public static void main(String[] args) {
+
+        Usuario usuario = new Usuario();
+        usuario.obterInformacoes();
+
+    }
 }
--- src/br/senai/sp/jandira/tabuada/model/Usuario.java
+++ src/br/senai/sp/jandira/tabuada/model/Usuario.java
@@ -1,5 +1,25 @@
 package br.senai.sp.jandira.tabuada.model;
 
+import java.util.Scanner;
+
 public class Usuario {
+
+    public int multiplicando;
+    public int multiplicadorInicial;
+    public int multiplicadorFinal;
+    public double[] tabuada;
+
+    public void obterInformacoes(){
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Qual o valor do multiplicando? ");
+        multiplicando = leitor.nextInt();
+
+        System.out.print("Qual o valor do multiplicador inicial? ");
+        multiplicadorInicial = leitor.nextInt();
+
+        System.out.print("Qual o valor do multiplicador final? ");
+        multiplicadorFinal = leitor.nextInt();
+
+    }
 
 }
